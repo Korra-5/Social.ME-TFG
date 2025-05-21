a801a8c9d403c7705cf254acd01c7eaf754e395b
Daniel Corral <dani05corral@gmail.com>
2025-05-21 19:52:33 +0200

    Arreglando chat

--- Social.ME/src/main/kotlin/com/example/socialme/dto/MensajeCreateDTO.kt
+++ Social.ME/src/main/kotlin/com/example/socialme/dto/MensajeCreateDTO.kt
@@ -1,12 +1,8 @@
 package com.example.socialme.dto
 
-import java.util.Date
-
 data class MensajeCreateDTO(
-    val id: String?,
     val comunidadUrl: String,
     val username: String,
-    val contenido: String,
-    val fechaEnvio: Date,
-    val leido: Boolean = false
+    val contenido: String
+    // Remove fechaEnvio from here - it will be set by the server
 )
